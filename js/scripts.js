a76c33c3ec0f5fea4af6499cdfe63775593b8b39
Alan Santos <alanxdsi@gmail.com>
2021-09-16 16:50:57 -0300

    4 parte

--- js/scripts.js
+++ js/scripts.js
@@ -32,15 +32,32 @@ _soundUrls.forEach(sndPath => {
 });
 
 _gui.switch.addEventListener("click", () => {
+	_data.gameOn = _gui.switch.classList.toggle("gui__btn-switch--on");
+	
+	_gui.counter.classList.toggle("gui__counter--on");
+	_gui.counter.innerHTML = "--";
 
+	_data.strict = false;
+	_data.playerCanPlay = false;
+	_data.score = 0; 
+	_data.gameSequence = [];
+	_data.playerSequence = [];
+
+	disablePads();
+
+	_gui.led.classList.remove("gui__led--active");
 });
 
 _gui.strict.addEventListener("click", () => {
+	if (!_data.gameOn)
+		return;
 
+	_data.strict = _gui.led.classList.toggle("gui__led--active");
+	
 });
 
 _gui.start.addEventListener("click", () => {
-
+	startGame();
 });
 
 const padListener = (e) => {
@@ -52,15 +69,22 @@ _gui.pads.forEach(pad => {
 });
 
 const startGame = () => {
-
+	blink("--", () => {
+		newColor();
+	})
 }
 
 const setScore = () => {
-
+	const score = _data.score.toString();
+	const display = "00".substring(0, 2 - score.length) + score;
+	_gui.counter.innerHTML = display;
 }
 
 const newColor = () => {
+	_data.gameSequence.push(Math.floor(Math.random() * 4));
+	_data.score++;
 
+	setScore();
 }
 
 const playSequence = () => {
@@ -68,7 +92,31 @@ const playSequence = () => {
 }
 
 const blink = (text, callback) => {
+	let counter = 0,
+		on = true;
 
+	_gui.counter.innerHTML = text;
+
+	const interval = setInterval(() => {
+		if(!_data.gameOn) {
+			clearInterval(interval);
+			_gui.counter.classList.remove("gui__counter--on");
+			return;
+		}
+		if(on) {
+			_gui.counter.classList.remove("gui__counter--on");
+		} else {
+			_gui.counter.classList.add("gui__counter--on");
+
+			if(++counter === 3) {
+				clearInterval(interval);
+				callback();
+			}
+		}
+
+		on = !on;
+
+	}, 250);
 }
 
 const waitForPlayerClick = () => {
@@ -84,5 +132,7 @@ const changePadCursor = (cursorType) => {
 }
 
 const disablePads = () => {
-
+	_gui.pads.forEach(pad => {
+		pad.classList.remove("game__pad--active");
+	})
 }
